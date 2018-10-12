941605d753552ec439beb32cd014e1bdd40ab1f4
David Lee <david.lee@spindriftgroup.com>
2018-10-12 21:41:13 +0200

    make configuration external

---
+++ componentConfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+  componentName: "occReactComponent",
+  dependencies: {
+    knockout: "knockout",
+    jquery: "jquery",
+    pubsub: "pubsub",
+    ccConstants: "ccConstants",
+    ccRestClient: "ccRestClient",
+    navigation: "navigation",
+    notifier: "notifier",
+    ccLogger: "ccLogger",
+    CCi18n: "CCi18n",
+    ccNumber: "ccNumber",
+    currencyHelper: "currencyHelper",
+    numberFormatHelper: "numberFormatHelper",
+    "pageLayout/product": "Product",
+    "ojs/ojcore": "ojs/ojcore",
+    "ojs/ojvalidation": "ojs/ojvalidation",
+    react: "https://unpkg.com/react@16.5.2/umd/react.production.min",
+    "react-dom":
+      "https://unpkg.com/react-dom@16.5.2/umd/react-dom.production.min",
+    redux: "https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.0/redux.min"
+  }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -23,27 +23,7 @@ const config = {
     // publicPath: "/file/widget/anotherWidget/js/",
     libraryTarget: "amd"
   },
-  externals: {
-    knockout: "knockout",
-    jquery: "jquery",
-    pubsub: "pubsub",
-    ccConstants: "ccConstants",
-    ccRestClient: "ccRestClient",
-    navigation: "navigation",
-    notifier: "notifier",
-    ccLogger: "ccLogger",
-    CCi18n: "CCi18n",
-    ccNumber: "ccNumber",
-    currencyHelper: "currencyHelper",
-    numberFormatHelper: "numberFormatHelper",
-    "pageLayout/product": "Product",
-    "ojs/ojcore": "ojs/ojcore",
-    "ojs/ojvalidation": "ojs/ojvalidation",
-    react: "https://unpkg.com/react@16.5.2/umd/react.production.min",
-    "react-dom":
-      "https://unpkg.com/react-dom@16.5.2/umd/react-dom.production.min",
-    redux: "https://cdnjs.cloudflare.com/ajax/libs/redux/4.0.0/redux.min"
-  },
+  externals: componentConfig.dependencies,
   devServer: {
     hot: true,
     publicPath: "/public/",
