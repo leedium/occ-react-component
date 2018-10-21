76c4570d84b26640258d6cc5a748bfee88d8640c
David Lee <david.lee@spindriftgroup.com>
2018-10-21 09:52:36 +0200

    adding file info

--- app/js/app/App.jsx
+++ app/js/app/App.jsx
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2018 LEEDIUM.
+ * This file is subject to the terms and conditions
+ * defined in file 'LICENSE.txt', which is part of this
+ * source code package.
+ */
+
+/**
+ * @project occ-react-component
+ * @file App.jsx
+ * @company leedium
+ * @createdBy davidlee
+ * @contact david@leedium.com
+ * @dateCreated 21/10/2018
+ * @description  Sample Application.. knock urself out from here...
+**/
+
+
 import React, {Component} from 'react';
 
 class App extends Component {
@@ -5,9 +23,7 @@ class App extends Component {
     super(props,context);
     console.log('Props:',this.props);
   }
-
   render () {
-
     return (
       <div>This is an Application</div>
     )
--- app/js/app/occProvider/OccProvider.jsx
+++ app/js/app/occProvider/OccProvider.jsx
@@ -5,13 +5,24 @@
  * source code package.
  */
 
+/**
+ * @project occ-react-component
+ * @file OccProvider.jsx
+ * @company leedium
+ * @createdBy davidlee
+ * @contact david@leedium.com
+ * @dateCreated 21/10/2018
+ * @description Higher order component to pass through OCC props to
+ *              Wrapped Component
+**/
+
+
 import React, {Component} from 'react';
 
 function OccProvider (WrappedComponent, occData) {
   return class extends Component {
     constructor (props, context) {
       super(props, context);
-      console.log(occData);
     }
     render () {
       const {children} = this.props;
