d4247e6b9d28165a3d0e99aa66128c3c32913afb
David Lee <david@leedium.com>
2019-01-04 09:52:16 +0100

    updated bundle js to prod

--- file/widget/occReactComponent/js/bundle.js
+++ file/widget/occReactComponent/js/bundle.js
@@ -10,14 +10,14 @@ define([
   "pageLayout/product"
 ], function(
   __WEBPACK_EXTERNAL_MODULE__3__,
+  __WEBPACK_EXTERNAL_MODULE__12__,
   __WEBPACK_EXTERNAL_MODULE__13__,
   __WEBPACK_EXTERNAL_MODULE__14__,
   __WEBPACK_EXTERNAL_MODULE__15__,
   __WEBPACK_EXTERNAL_MODULE__16__,
   __WEBPACK_EXTERNAL_MODULE__17__,
   __WEBPACK_EXTERNAL_MODULE__18__,
-  __WEBPACK_EXTERNAL_MODULE__19__,
-  __WEBPACK_EXTERNAL_MODULE__20__
+  __WEBPACK_EXTERNAL_MODULE__19__
 ) {
   return /******/ (function(modules) {
     // webpackBootstrap
@@ -111,7 +111,7 @@ define([
     }
     /******/
     /******/ var hotApplyOnUpdate = true; // eslint-disable-next-line no-unused-vars
-    /******/ /******/ var hotCurrentHash = "18d0389afc2e6a28bfb8";
+    /******/ /******/ var hotCurrentHash = "50b73c4be42ddd164be8";
     /******/ var hotRequestTimeout = 10000;
     /******/ var hotCurrentModuleData = {};
     /******/ var hotCurrentChildModule; // eslint-disable-next-line no-unused-vars
@@ -1114,14 +1114,14 @@ define([
          *              React component props
          */
         !((__WEBPACK_AMD_DEFINE_ARRAY__ = [
+          __webpack_require__(12),
           __webpack_require__(13),
           __webpack_require__(14),
           __webpack_require__(15),
           __webpack_require__(16),
           __webpack_require__(17),
           __webpack_require__(18),
-          __webpack_require__(19),
-          __webpack_require__(20)
+          __webpack_require__(19)
         ]),
         (__WEBPACK_AMD_DEFINE_RESULT__ = function def(
           ko,
@@ -1173,7 +1173,7 @@ define([
       /* 7 */
       /***/ function(module, exports, __webpack_require__) {
         "use strict";
-        /* WEBPACK VAR INJECTION */ (function(module, process) {
+        /* WEBPACK VAR INJECTION */ (function(module) {
           Object.defineProperty(exports, "__esModule", {
             value: true
           });
@@ -1182,13 +1182,13 @@ define([
 
           var _react2 = _interopRequireDefault(_react);
 
-          var _root = __webpack_require__(9);
+          var _root = __webpack_require__(8);
 
-          var _Product = __webpack_require__(10);
+          var _Product = __webpack_require__(9);
 
           var _Product2 = _interopRequireDefault(_Product);
 
-          var _ProductList = __webpack_require__(12);
+          var _ProductList = __webpack_require__(11);
 
           var _ProductList2 = _interopRequireDefault(_ProductList);
 
@@ -1259,8 +1259,7 @@ define([
             );
           };
 
-          var _default =
-            process.env === "development" ? (0, _root.hot)(App) : App;
+          var _default = (0, _root.hot)(App);
 
           exports.default = _default;
           (function() {
@@ -1287,228 +1286,11 @@ define([
 
           module.exports = exports.default;
           /* WEBPACK VAR INJECTION */
-        }.call(this, __webpack_require__(2)(module), __webpack_require__(8)));
+        }.call(this, __webpack_require__(2)(module)));
 
         /***/
       },
       /* 8 */
-      /***/ function(module, exports) {
-        // shim for using process in browser
-        var process = (module.exports = {}); // cached from whatever global is present so that test runners that stub it
-        // don't break things.  But we need to wrap it in a try catch in case it is
-        // wrapped in strict mode code which doesn't define any globals.  It's inside a
-        // function because try/catches deoptimize in certain engines.
-
-        var cachedSetTimeout;
-        var cachedClearTimeout;
-
-        function defaultSetTimout() {
-          throw new Error("setTimeout has not been defined");
-        }
-
-        function defaultClearTimeout() {
-          throw new Error("clearTimeout has not been defined");
-        }
-
-        (function() {
-          try {
-            if (typeof setTimeout === "function") {
-              cachedSetTimeout = setTimeout;
-            } else {
-              cachedSetTimeout = defaultSetTimout;
-            }
-          } catch (e) {
-            cachedSetTimeout = defaultSetTimout;
-          }
-
-          try {
-            if (typeof clearTimeout === "function") {
-              cachedClearTimeout = clearTimeout;
-            } else {
-              cachedClearTimeout = defaultClearTimeout;
-            }
-          } catch (e) {
-            cachedClearTimeout = defaultClearTimeout;
-          }
-        })();
-
-        function runTimeout(fun) {
-          if (cachedSetTimeout === setTimeout) {
-            //normal enviroments in sane situations
-            return setTimeout(fun, 0);
-          } // if setTimeout wasn't available but was latter defined
-
-          if (
-            (cachedSetTimeout === defaultSetTimout || !cachedSetTimeout) &&
-            setTimeout
-          ) {
-            cachedSetTimeout = setTimeout;
-            return setTimeout(fun, 0);
-          }
-
-          try {
-            // when when somebody has screwed with setTimeout but no I.E. maddness
-            return cachedSetTimeout(fun, 0);
-          } catch (e) {
-            try {
-              // When we are in I.E. but the script has been evaled so I.E. doesn't trust the global object when called normally
-              return cachedSetTimeout.call(null, fun, 0);
-            } catch (e) {
-              // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error
-              return cachedSetTimeout.call(this, fun, 0);
-            }
-          }
-        }
-
-        function runClearTimeout(marker) {
-          if (cachedClearTimeout === clearTimeout) {
-            //normal enviroments in sane situations
-            return clearTimeout(marker);
-          } // if clearTimeout wasn't available but was latter defined
-
-          if (
-            (cachedClearTimeout === defaultClearTimeout ||
-              !cachedClearTimeout) &&
-            clearTimeout
-          ) {
-            cachedClearTimeout = clearTimeout;
-            return clearTimeout(marker);
-          }
-
-          try {
-            // when when somebody has screwed with setTimeout but no I.E. maddness
-            return cachedClearTimeout(marker);
-          } catch (e) {
-            try {
-              // When we are in I.E. but the script has been evaled so I.E. doesn't  trust the global object when called normally
-              return cachedClearTimeout.call(null, marker);
-            } catch (e) {
-              // same as above but when it's a version of I.E. that must have the global object for 'this', hopfully our context correct otherwise it will throw a global error.
-              // Some versions of I.E. have different rules for clearTimeout vs setTimeout
-              return cachedClearTimeout.call(this, marker);
-            }
-          }
-        }
-
-        var queue = [];
-        var draining = false;
-        var currentQueue;
-        var queueIndex = -1;
-
-        function cleanUpNextTick() {
-          if (!draining || !currentQueue) {
-            return;
-          }
-
-          draining = false;
-
-          if (currentQueue.length) {
-            queue = currentQueue.concat(queue);
-          } else {
-            queueIndex = -1;
-          }
-
-          if (queue.length) {
-            drainQueue();
-          }
-        }
-
-        function drainQueue() {
-          if (draining) {
-            return;
-          }
-
-          var timeout = runTimeout(cleanUpNextTick);
-          draining = true;
-          var len = queue.length;
-
-          while (len) {
-            currentQueue = queue;
-            queue = [];
-
-            while (++queueIndex < len) {
-              if (currentQueue) {
-                currentQueue[queueIndex].run();
-              }
-            }
-
-            queueIndex = -1;
-            len = queue.length;
-          }
-
-          currentQueue = null;
-          draining = false;
-          runClearTimeout(timeout);
-        }
-
-        process.nextTick = function(fun) {
-          var args = new Array(arguments.length - 1);
-
-          if (arguments.length > 1) {
-            for (var i = 1; i < arguments.length; i++) {
-              args[i - 1] = arguments[i];
-            }
-          }
-
-          queue.push(new Item(fun, args));
-
-          if (queue.length === 1 && !draining) {
-            runTimeout(drainQueue);
-          }
-        }; // v8 likes predictible objects
-
-        function Item(fun, array) {
-          this.fun = fun;
-          this.array = array;
-        }
-
-        Item.prototype.run = function() {
-          this.fun.apply(null, this.array);
-        };
-
-        process.title = "browser";
-        process.browser = true;
-        process.env = {};
-        process.argv = [];
-        process.version = ""; // empty string to avoid regexp issues
-
-        process.versions = {};
-
-        function noop() {}
-
-        process.on = noop;
-        process.addListener = noop;
-        process.once = noop;
-        process.off = noop;
-        process.removeListener = noop;
-        process.removeAllListeners = noop;
-        process.emit = noop;
-        process.prependListener = noop;
-        process.prependOnceListener = noop;
-
-        process.listeners = function(name) {
-          return [];
-        };
-
-        process.binding = function(name) {
-          throw new Error("process.binding is not supported");
-        };
-
-        process.cwd = function() {
-          return "/";
-        };
-
-        process.chdir = function(dir) {
-          throw new Error("process.chdir is not supported");
-        };
-
-        process.umask = function() {
-          return 0;
-        };
-
-        /***/
-      },
-      /* 9 */
       /***/ function(module, exports, __webpack_require__) {
         /* WEBPACK VAR INJECTION */ (function(module) {
           if (true) {
@@ -1535,7 +1317,7 @@ define([
 
         /***/
       },
-      /* 10 */
+      /* 9 */
       /***/ function(module, exports, __webpack_require__) {
         "use strict";
         /* WEBPACK VAR INJECTION */ (function(module) {
@@ -1551,7 +1333,7 @@ define([
 
           var _styledComponents2 = _interopRequireDefault(_styledComponents);
 
-          var _SkuThumb = __webpack_require__(11);
+          var _SkuThumb = __webpack_require__(10);
 
           var _SkuThumb2 = _interopRequireDefault(_SkuThumb);
 
@@ -1704,7 +1486,7 @@ define([
                           return _react2.default.createElement(
                             _SkuThumb2.default,
                             {
-                              key: item.id,
+                              key: item.repositoryId,
                               itemData: item,
                               handleClick: _this3.skuSelected
                             }
@@ -1765,7 +1547,7 @@ define([
 
         /***/
       },
-      /* 11 */
+      /* 10 */
       /***/ function(module, exports, __webpack_require__) {
         "use strict";
         /* WEBPACK VAR INJECTION */ (function(module) {
@@ -1836,13 +1618,11 @@ define([
               _proto.render = function render() {
                 var _this$props = this.props,
                   itemData = _this$props.itemData,
-                  handleClick = _this$props.handleClick,
-                  key = _this$props.key;
+                  handleClick = _this$props.handleClick;
                 return _react2.default.createElement(
                   StyledSkuThumb,
                   {
                     type: "button",
-                    key: key,
                     onClick: function onClick() {
                       return handleClick(itemData);
                     }
@@ -1902,7 +1682,7 @@ define([
 
         /***/
       },
-      /* 12 */
+      /* 11 */
       /***/ function(module, exports, __webpack_require__) {
         "use strict";
         /* WEBPACK VAR INJECTION */ (function(module) {
@@ -2034,6 +1814,12 @@ define([
 
         /***/
       },
+      /* 12 */
+      /***/ function(module, exports) {
+        module.exports = __WEBPACK_EXTERNAL_MODULE__12__;
+
+        /***/
+      },
       /* 13 */
       /***/ function(module, exports) {
         module.exports = __WEBPACK_EXTERNAL_MODULE__13__;
@@ -2073,12 +1859,6 @@ define([
       /* 19 */
       /***/ function(module, exports) {
         module.exports = __WEBPACK_EXTERNAL_MODULE__19__;
-
-        /***/
-      },
-      /* 20 */
-      /***/ function(module, exports) {
-        module.exports = __WEBPACK_EXTERNAL_MODULE__20__;
 
         /***/
       }
